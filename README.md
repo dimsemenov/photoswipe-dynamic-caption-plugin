611275e3a2a35a6dbf98d63334301e6a72eaa9ba
Dmitry Semenov <diiiimaaaa@gmail.com>
2021-07-19 08:39:33 +0300

    Initial commit
    initial
    
    work work
    
    work work
    
    descriptive summary
    
    Update README.md
    
    Update README.md
    
    Update README.md

---
+++ README.md
@@ -0,0 +1,116 @@
+The caption plugin for PhotoSwipe v5, can automatically position the text below or aside depending on the available space. Recommended for small to medium sized captions.
+
+## Intiailization
+
+The plugin has a single JS `photoswipe-dynamic-caption-plugin.esm.js` and a single CSS file `photoswipe-dynamic-caption-plugin.css`.
+
+It can be initialized like this:
+
+```html
+<script type="module">
+import PhotoSwipeLightbox from './lib/photoswipe/photoswipe-lightbox.esm.js';
+import PhotoSwipeDynamicCaption from './photoswipe-dynamic-caption-plugin.esm.js';
+
+const lightbox = new PhotoSwipeLightbox({
+  gallerySelector: '#gallery',
+  childSelector: '.pswp-gallery__item',
+  
+  // Optional padding for images
+  paddingTop: 30,
+  paddingBottom: 30,
+  paddingLeft: 70,
+  paddingRight: 70,
+
+  pswpModule: './photoswipe.esm.js'
+});
+
+const captionPlugin = new PhotoSwipeDynamicCaption(lightbox, {
+  // Plugins options, for example:
+  type: 'auto',
+});
+
+// make sure you init photoswipe core after plugins are added
+lightbox.init();
+</script>
+```
+
+
+
+## Plugin options
+
+#### `captionContent: '.pswp-caption-content'`
+
+Element class from which caption content will be retrieved, if element is not found - the plugin will try to use thumbnail image `alt` attribute.
+
+Or a function that should return caption content. For example:
+ 
+```js
+captionContent: (slide) => {
+  return slide.data.element.querySelector('img').getAttribute('alt');
+}
+```
+
+#### `type: 'auto'`
+
+Position type of the caption, can be 'auto', 'below', or 'aside'.
+
+- 'below' - caption will always be place below the image
+- 'aside' - caption will always be placed to the right side of the image
+- 'auto'  - the plugin will try to automatically determine the best position (depending on available space)
+
+#### `mobileLayoutBreakpoint: 600`
+
+Maximum window width at which mobile layout should be used, or a function that should return true if mobile layou should be used. For example:
+  
+```js
+mobileLayoutBreakpoint: (pswp, captionPlugin) => {
+  return (window.innerWidth < 750);
+}
+```
+
+#### `disableImagePaddingOnMobile: true`
+
+Automatically disable paddingTop/Left/Right/Bottom when mobile layout is used.
+  
+
+## Styling
+
+- The caption has class `pswp__dynamic-caption`, additionally:
+  - while it's below the main image: `pswp__dynamic-caption--below`
+  - while it's aside: `pswp__dynamic-caption--aside`
+  - while mobile layout is used: `pswp__dynamic-caption--mobile`
+
+Feel free to adjust it in the plugin CSS file:
+
+```css
+.pswp__dynamic-caption--aside {
+  max-width: 300px;
+  padding: 20px 15px 20px 20px;
+  margin-top: 70px;
+}
+.pswp__dynamic-caption--below {
+  max-width: 700px;
+  padding: 15px 0 0;
+}
+.pswp__dynamic-caption--mobile {
+  background: rgba(0,0,0,0.5);
+  padding: 10px 15px;
+}
+```
+
+## How 'auto' positioning works
+
+- Check if there is more horizontal or vertical free space around the image.
+- If there is more free vertical space:
+  - Set caption width to the width of the image 
+  - Add `pswp__dynamic-caption--below` class, so size can also be adjusted via CSS.
+  - Measure caption height.
+  - Check if caption will fit without any adjustments of image position.
+    - If it does - just show the caption below the image.
+    - If it doesn't - reduce pan area height by the height of caption.
+- If there is more horizontal space:
+  - Add `pswp__dynamic-caption--aside` class, so size can be adjusted via CSS.
+  - Measure caption width.
+  - Check if caption will fit on the right side without any adjustments of image position.
+    - If it does - just show the caption aside of the image.
+    - If it doesn't - reduce pan area width by the width of caption.
---
+++ photoswipe-dynamic-caption-plugin.esm.js
@@ -0,0 +1,322 @@
+const defaultOptions = {
+  captionContent: '.pswp-caption-content',
+  type: 'auto',
+  mobileLayoutBreakpoint: 600,
+  disableImagePaddingOnMobile: true
+};
+
+
+class PhotoSwipeDynamicCaption {
+  constructor(lightbox, options) {
+    this.options = {
+      ...defaultOptions,
+      ...options
+    };
+
+    this.lightbox = lightbox;
+
+    this.lightbox.on('init', () => {
+      this.initPlugin();
+    });
+  }
+
+  initPlugin() {
+    this.pswp = this.lightbox.pswp;
+    this.isCaptionHidden = false;
+    this.tempCaption = false;
+    this.captionElement = false;
+
+    // store initial padding that's defined during initialization
+    this.initialPadding = {
+      left: this.pswp.options.paddingLeft,
+      bottom: this.pswp.options.paddingBottom,
+      top: this.pswp.options.paddingTop,
+      right: this.pswp.options.paddingRight
+    };
+
+    if (this.useMobileLayout()) {
+      this.disableSlidePadding();
+    }
+
+    this.pswp.on('uiRegister', () => {
+      this.pswp.ui.registerElement({
+        name: 'dynamic-caption',
+        order: 9,
+        isButton: false,
+        appendTo: 'root',
+        html: '',
+        onInit: (el) => {
+          this.captionElement = el;
+          this.initCaption();
+        }
+      });
+    });
+  }
+
+  initCaption() {
+    const { pswp } = this;
+
+    pswp.on('change', () => {
+      this.updateCaptionHTML(); 
+      this.updateCurrentCaptionPosition();
+
+      // make sure caption is displayed after slides are switched
+      this.showCaption();
+    });
+
+    pswp.on('calcSlideSize', (e) => this.onCalcSlideSize(e));
+
+    // hide caption if mainscroll is shifted (dragging)
+    pswp.on('moveMainScroll', () => {
+      if (!this.useMobileLayout()) {
+        if (this.pswp.mainScroll.isShifted()) {
+          this.hideCaption();
+        } else {
+          this.showCaption();
+        }
+      }
+    });
+
+    // hide caption if zoomed
+    pswp.on('zoomPanUpdate', () => {
+      if (pswp.currSlide.currZoomLevel > pswp.currSlide.zoomLevels.initial) {
+        this.hideCaption();
+      } else {
+        this.showCaption();
+      }
+    });
+  }
+
+  useMobileLayout() {
+    const { mobileLayoutBreakpoint } = this.options;
+
+    if (typeof mobileLayoutBreakpoint === 'function') {
+      return mobileLayoutBreakpoint.call(this);
+    } else if (typeof mobileLayoutBreakpoint === 'number') {
+      if (window.innerWidth < mobileLayoutBreakpoint) {
+        return true;
+      }
+    }
+    
+    return false;
+  }
+
+  hideCaption() {
+    if (!this.isCaptionHidden) {
+      this.isCaptionHidden = true;
+      this.captionElement.classList.add('pswp__dynamic-caption--faded');
+    }
+  }
+
+  showCaption() {
+    if (this.isCaptionHidden) {
+      this.isCaptionHidden = false;
+      this.captionElement.classList.remove('pswp__dynamic-caption--faded');
+    }
+  }
+
+  setCaptionPosition(x, y) {
+    this.captionElement.style.left = x + 'px';
+    this.captionElement.style.top = y + 'px';
+  }
+
+  setCaptionWidth(captionEl, width) {
+    if (!width) {
+      captionEl.style.removeProperty('width');
+    } else {
+      captionEl.style.width = width + 'px';
+    }
+  }
+
+  setCaptionType(captionEl, type) {
+    const prevType = captionEl.dataset.pswpCaptionType;
+    if (type !== prevType) {
+      captionEl.classList.add('pswp__dynamic-caption--' + type);
+      captionEl.classList.remove('pswp__dynamic-caption--' + prevType);
+      captionEl.dataset.pswpCaptionType = type;
+    }
+  }
+
+  updateCurrentCaptionPosition() {
+    const slide = this.pswp.currSlide;
+
+    if (!slide.dynamicCaptionType) {
+      return;
+    }
+
+    if (slide.dynamicCaptionType === 'mobile') {
+      this.setCaptionType(this.captionElement, slide.dynamicCaptionType);
+      
+      this.captionElement.style.removeProperty('left');
+      this.captionElement.style.removeProperty('top');
+      this.setCaptionWidth(this.captionElement, false);
+      return;
+    }
+
+    const zoomLevel = slide.zoomLevels.initial;
+    const imageWidth = Math.ceil(slide.width * zoomLevel);
+    const imageHeight = Math.ceil(slide.height * zoomLevel);
+
+    
+    this.setCaptionType(this.captionElement, slide.dynamicCaptionType);
+    if (slide.dynamicCaptionType === 'aside') {
+      this.setCaptionPosition(
+        this.pswp.currSlide.bounds.center.x + imageWidth,
+        this.pswp.currSlide.bounds.center.y
+      );
+      this.setCaptionWidth(this.captionElement, false);
+    } else if (slide.dynamicCaptionType === 'below') {
+      this.setCaptionPosition(
+        this.pswp.currSlide.bounds.center.x,
+        this.pswp.currSlide.bounds.center.y + imageHeight
+      );
+      this.setCaptionWidth(this.captionElement, imageWidth);
+    }
+  }
+
+  /**
+   * Temporary caption is used to measure size for the current/next/previous captions,
+   * (it has visibility:hidden)
+   */
+  createTemporaryCaption() {
+    this.tempCaption = document.createElement('div');
+    this.tempCaption.className = 'pswp__dynamic-caption pswp__dynamic-caption--temp';
+    this.tempCaption.style.visibility = 'hidden';
+    this.tempCaption.setAttribute('aria-hidden', 'true');
+    this.pswp.template.appendChild(this.tempCaption);
+  }
+
+  onCalcSlideSize(e) {
+    const { slide } = e;
+
+    const captionHTML = this.getCaptionHTML(e.slide);
+
+    if (!captionHTML) {
+      slide.dynamicCaptionType = false;
+      return;
+    }
+
+    if (this.useMobileLayout()) {
+      this.disableSlidePadding();
+      slide.dynamicCaptionType = 'mobile';
+    } else {
+      this.enableSlidePadding();
+      slide.bounds.update(slide.zoomLevels.initial);
+
+      if (this.options.type === 'auto') {
+        if (slide.bounds.center.x > slide.bounds.center.y) {
+          slide.dynamicCaptionType = 'aside';
+        } else {
+          slide.dynamicCaptionType = 'below';
+        }
+      } else {
+        slide.dynamicCaptionType = this.options.type;
+      }
+    } 
+
+    const imageWidth = Math.ceil(slide.width * slide.zoomLevels.initial);
+    const imageHeight = Math.ceil(slide.height * slide.zoomLevels.initial);
+
+    if (!this.tempCaption) {
+      this.createTemporaryCaption();
+    }
+
+    this.setCaptionType(this.tempCaption, slide.dynamicCaptionType);
+
+    if (slide.dynamicCaptionType === 'aside') {
+      this.tempCaption.innerHTML = this.getCaptionHTML(e.slide);
+      this.setCaptionWidth(this.tempCaption, false);
+      const captionWidth = this.tempCaption.getBoundingClientRect().width;
+
+      const horizontalEnding = imageWidth + slide.bounds.center.x;
+      const horizontalLeftover = (slide.panAreaSize.x - horizontalEnding);
+
+      if (horizontalLeftover <= captionWidth) {
+        slide.panAreaSize.x -= captionWidth;
+        slide.zoomLevels.update(slide.width, slide.height, slide.panAreaSize);
+        slide.bounds.update(slide.zoomLevels.initial);
+      } else {
+        // do nothing, caption will fit aside without any adjustments
+      }
+    } else if (slide.dynamicCaptionType === 'below') {
+      this.setCaptionWidth(this.tempCaption, imageWidth);
+      this.tempCaption.innerHTML = this.getCaptionHTML(e.slide);
+      const captionHeight = this.tempCaption.getBoundingClientRect().height;
+
+
+      // vertical ending of the image
+      const verticalEnding = imageHeight + slide.bounds.center.y;
+
+      // height between bottom of the screen and ending of the image
+      // (before any adjustments applied)
+      const verticalLeftover = slide.panAreaSize.y - verticalEnding;
+
+      if (verticalLeftover <= captionHeight) {
+        // lift up the image to give more space for caption
+        slide.panAreaSize.y -= captionHeight;
+        // we reduce viewport size, thus we need to update zoom level and pan bounds
+        slide.zoomLevels.update(slide.width, slide.height, slide.panAreaSize);
+        slide.bounds.update(slide.zoomLevels.initial);
+      } else {
+        // do nothing, caption will fit below the image without any adjustments
+      }
+    } else {
+      // mobile
+    }
+
+    if (slide === this.pswp.currSlide) {
+      this.updateCurrentCaptionPosition();
+    }
+  }
+
+  enableSlidePadding() {
+    if (this.options.disableImagePaddingOnMobile) {
+      const pswpOptions = this.pswp.options;
+      pswpOptions.paddingLeft = this.initialPadding.left;
+      pswpOptions.paddingRight = this.initialPadding.right;
+      pswpOptions.paddingTop = this.initialPadding.top;
+      pswpOptions.paddingBottom = this.initialPadding.bottom;
+    }
+  }
+
+  disableSlidePadding() {
+    if (this.options.disableImagePaddingOnMobile) {
+      const pswpOptions = this.pswp.options;
+      pswpOptions.paddingLeft = 0;
+      pswpOptions.paddingRight = 0;
+      pswpOptions.paddingTop = 0;
+      pswpOptions.paddingBottom = 0;
+    }
+  }
+
+  getCaptionHTML(slide) {
+    if (typeof this.options.captionContent === 'function') {
+      return this.options.captionContent.call(this, slide);
+    }
+
+    const currSlideElement = slide.data.element;
+    let captionHTML = '';
+    if (currSlideElement) {
+      const hiddenCaption = currSlideElement.querySelector(this.options.captionContent);
+      if (hiddenCaption) {
+        // get caption from element with class pswp-caption-content
+        captionHTML = hiddenCaption.innerHTML;
+      } else {
+        const img = currSlideElement.querySelector('img');
+        if (img) {
+          // get caption from alt attribute
+          captionHTML = img.getAttribute('alt');
+        }
+      }
+    }
+    return captionHTML;
+  }
+
+  updateCaptionHTML() {
+    const captionHTML = this.getCaptionHTML(pswp.currSlide);
+    this.captionElement.style.visibility = captionHTML ? 'visible' :  'hidden';
+    this.captionElement.innerHTML = captionHTML || '';
+  }
+}
+
+export default PhotoSwipeDynamicCaption;
